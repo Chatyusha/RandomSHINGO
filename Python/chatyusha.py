b1bba7a210246ebcf3bef66b46f7a1420e49d4a6
Chatyusha <hirounicat76@gmail.com>
2022-08-01 21:16:39 +0900

    ShinGO

---
+++ Python/chatyusha.py
@@ -0,0 +1,31 @@
+from math import fabs
+import random
+
+DDSK = ["ドド","スコ"]
+ddsk_tmp = "ドドスコスコスコ" * 3
+
+judge = ""
+
+flag = False
+
+for i in range(12):
+    t = random.randint(0,1) % 2
+    print(DDSK[t])
+    judge += DDSK[t]
+
+if judge == ddsk_tmp:
+    print("ラブ注入♡")
+    flag = True
+
+n = 1
+
+while(flag == False):
+    t = random.randint(0,1) % 2
+    print(DDSK[t])
+    judge += DDSK[t]
+
+    if judge[2*n:] == ddsk_tmp:
+        print("ラブ注入♡")
+        flag = True
+    
+    n += 1
